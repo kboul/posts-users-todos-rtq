4d4b10686aa928013846b2c0e159da34dc210e8d
kboul <kos.voulgaridis@gmail.com>
2022-09-29 18:14:53 +0300

    Bring posts with date to the frond and sort them by date

--- src/app/services/posts.ts
+++ src/app/services/posts.ts
@@ -7,6 +7,8 @@ export const postsApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getPosts: builder.query<PostsResponse, void>({
       query: () => "posts",
+      transformResponse: (res: Post[]) =>
+        res.sort((a, b) => Date.parse(b.date) - Date.parse(a.date)),
       providesTags: ["Post"]
     })
   })
