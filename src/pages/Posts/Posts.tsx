a845af8d1cbe0bcb0b755a9367ac758b11157aff
kboul <kos.voulgaridis@gmail.com>
2022-10-02 19:28:07 +0300

    Push date to the end of card

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -4,10 +4,10 @@ import Post from "./model";
 
 const className = {
   container:
-    "justify-between flex flex-row gap-4 flex-wrap after:content-[''] after:flex-auto p-4",
+    "justify-between flex flex-row gap-4 flex-wrap after:content-[''] after:flex-auto p-[5px]",
   card: "relative border border-gray-400 shadow-md bg-white w-[270px] h-[120px] rounded-md p-2 flex flex-col", // two last properties needed to apply the last styles on price to be at the bottom of the card
   title: "font-bold text-center",
-  body: "mx-2 my-2 text-sm",
+  body: "mx-2 my-2 text-sm flex-1",
   date: "text-right text-xs italic"
 };
 
